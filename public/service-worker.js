416e3b78d4c6d0f68073942bd884fa1c4dd350f7
Jon Sanchez <jonathan.sanchez@windstream.net>
2021-01-14 14:31:41 -0700

    Heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,16 +5,18 @@ const FILES_TO_CACHE = [
     "./css/styles.css",
     "./js/index.js",
     "./js/idb.js",
-    "./icons/icon-72x72.png",
+    /*"./icons/icon-72x72.png",
     "./icons/icon-96x96.png",
     "./icons/icon-128x128.png",
     "./icons/icon-144x144.png",
-    "./icons/icon-152x152.png",
+    "./icons/icon-152x152.png",*/
 
     //192X192 is only being used
     "./icons/icon-192x192.png",
+    /*
     "./icons/icon-384x384.png",
-    "./icons/icon-512x512.png",
+
+    "./icons/icon-512x512.png",*/
     "./manifest.json"
 ];
 
